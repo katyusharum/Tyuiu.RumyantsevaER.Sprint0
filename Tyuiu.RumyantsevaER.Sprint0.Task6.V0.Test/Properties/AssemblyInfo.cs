ae6e7ed3893d5b3aac5748f223798f238844c8ba
katyusharum <tyuiu.rumyantsevaer@outlook.com>
2023-12-24 18:31:35 +0500

    task5

---
+++ Tyuiu.RumyantsevaER.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.RumyantsevaER.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.RumyantsevaER.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e14a2168-c5bb-4b48-b230-ec3d56b1dcf1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.RumyantsevaER.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.RumyantsevaER.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.RumyantsevaER.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //линейная
+            Console.WriteLine("А + В = " + DataService.Addition(1, 5));
+            Console.WriteLine("А - В = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("А * В = " + DataService.Multiplication(10, 10));
+            //разветвляющаяся
+            Console.WriteLine("А / В = " + DataService.Division(9, 3));
+            Console.ReadKey();
+        }
+    }
+}
+
